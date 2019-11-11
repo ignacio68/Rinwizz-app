4ab32874dd15b707615023b07418ba08f6393bc4
ignacio68 <ilopez-amor@ya.com>
2019-11-11 18:58:36 +0100

    feat: minor changes

--- app/main.js
+++ app/main.js
@@ -1,18 +1,17 @@
-import Vue from 'nativescript-vue'
-import App from './components/App'
+import Vue from "nativescript-vue"
+import App from "./components/App"
 
-import VueDevtools from 'nativescript-vue-devtools'
-import store from './store'
+import store from "./store"
+import VueDevtools from "nativescript-vue-devtools"
 
-if(TNS_ENV !== 'production') {
+if (TNS_ENV !== "production") {
   Vue.use(VueDevtools)
 }
-  
-// Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = (TNS_ENV === 'production')
 
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = TNS_ENV === "production"
 
 new Vue({
   store,
-  render: h => h('frame', [h(App)])
+  render: h => h("frame", [h(App)])
 }).$start()
