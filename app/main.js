79072cc633ecc5fe01ce09a17904be8436ccc0ac
Ignacio <buscemi@ya.com>
2019-11-12 11:03:36 +0100

    feat: minor changes

--- app/main.js
+++ app/main.js
@@ -1,26 +1,26 @@
-import Vue from "nativescript-vue"
+import Vue from "nativescript-vue";
 
 // Add view components
-import Welcome from "./views/Welcome"
-import AppSplitter from "./views/AppSplitter"
+import Welcome from "./views/Welcome";
+import AppSplitter from "./views/AppSplitter";
+
+import store from "./store";
+import VueDevtools from "nativescript-vue-devtools";
 
 //Add UI components
-import RadSideDrawer from "nativescript-ui-sidedrawer/vue"
-
-import store from "./store"
-import VueDevtools from "nativescript-vue-devtools"
+import RadSideDrawer from "nativescript-ui-sidedrawer/vue";
 
 if (TNS_ENV !== "production") {
-  Vue.use(VueDevtools)
+  Vue.use(VueDevtools);
 }
 
-Vue.use(RadSideDrawer)
+Vue.use(RadSideDrawer);
 // Vue.component("RadSideDrawer", RadSideDrawer)
 
 // Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = TNS_ENV === "production"
+Vue.config.silent = TNS_ENV === "production";
 
 new Vue({
   store,
   render: h => h("frame", [h(AppSplitter)])
-}).$start()
+}).$start();
