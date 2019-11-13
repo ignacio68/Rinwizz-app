e7e135d0a3e708d02fc67fc14242f80abbe8daca
ignacio68 <ilopez-amor@ya.com>
2019-11-13 09:36:08 +0100

    NEW: Create search page

---
+++ app/views/Search/Search.vue
@@ -0,0 +1,25 @@
+<template>
+  <Page>
+    <ActionBar title="Search" />
+    <StackLayout>
+      <Label class="text" text="Search page" textWrap="true" />
+    </StackLayout>
+  </Page>
+</template>
+<script>
+export default {
+  name: 'Search',
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+<style scoped>
+.text {
+  vertical-align: center;
+  text-align: center;
+  margin-top: 16px;
+  font-size: 20;
+}
+</style>
