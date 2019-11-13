535fe10fdb23a58aafc01e92cd11ae8541e02af4
Ignacio <buscemi@ya.com>
2019-11-13 11:51:31 +0100

    NEW: create AlertMessage  view

---
+++ app/views/Alerts/AlertEditor.vue
@@ -0,0 +1,33 @@
+<template>
+  <Page>
+    <ActionBar title="Alert Editor" />
+    <StackLayout backgroundColor="#4A536B">
+      <Label class="text" text="AlertEditor page" textWrap="true" />
+      <Label class="text" :text="message" textWrap="true" />
+      <alertScript />
+      <Button text="Cancelar" @tap="$modal.close" />
+      <Button text="Ok" @tap="$modal.close" />
+    </StackLayout>
+  </Page>
+</template>
+<script>
+import alertScript from '../../components/Alerts/alertScript'
+export default {
+  name: 'AlertEditor',
+  components: {
+    alertScript
+  },
+  props: ['message'],
+  data() {
+    return {}
+  }
+}
+</script>
+<style scoped>
+.text {
+  vertical-align: center;
+  text-align: center;
+  margin-top: 16px;
+  font-size: 20;
+}
+</style>
--- app/views/Alerts/Alerts.vue
+++ app/views/Alerts/Alerts.vue
@@ -3,14 +3,24 @@
     <ActionBar title="Alerts" />
     <StackLayout backgroundColor="#58e821">
       <Label class="text" text="Alerts page" textWrap="true" />
+      <Button text="Alert Editor" @tap="openAlertModal" />
     </StackLayout>
   </Page>
 </template>
 <script>
+import AlertEditor from './AlertEditor'
 export default {
   name: 'Alerts',
   data() {
     return {}
+  },
+  methods: {
+    openAlertModal() {
+      this.$showModal(AlertEditor, {
+        fullscreen: true,
+        props: { message: 'Todo va dabuten' }
+      })
+    }
   }
 }
 </script>
