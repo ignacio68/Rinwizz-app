db26edf0eac3ca269c114f4e85075040ec97c0c4
Ignacio <buscemi@ya.com>
2019-11-15 12:16:37 +0100

    IMPROVE: Add component path alias

--- app/views/Alerts/AlertEditor.vue
+++ app/views/Alerts/AlertEditor.vue
@@ -5,23 +5,23 @@
       <Label class="text" text="AlertEditor page" textWrap="true" />
       <Label class="text" :text="message" textWrap="true" />
       <alertScript />
-      <Button text="Cancelar" @tap="$modal.close" />
+      <Button text="Cancelar" @tap="$modal.close" class="cancelButton" />
       <Button text="Ok" @tap="$modal.close" />
     </StackLayout>
   </Page>
 </template>
 <script>
 // TODO: revisar @component alias
-import alertScript from '../../components/Alerts/alertScript'
+import alertScript from '@components/Alerts/alertScript'
 export default {
   name: 'AlertEditor',
   components: {
-    alertScript
+    alertScript,
   },
   props: ['message'],
   data() {
     return {}
-  }
+  },
 }
 </script>
 <style scoped>
@@ -30,5 +30,9 @@ export default {
   text-align: center;
   margin-top: 16px;
   font-size: 20;
+}
+.cancelButton {
+  background-color: firebrick;
+  color: wheat;
 }
 </style>
