c2e603ae9884e95043c10dfc26305d6806a249aa
ignacio68 <ilopez-amor@ya.com>
2019-11-20 09:55:41 +0100

    NEW: Add dataField component

---
+++ app/components/Auth/dataField.vue
@@ -0,0 +1,57 @@
+<template>
+  <StackLayout orientation="horizontal">
+    <Image :src="imageSrc" stretch="none" />
+    <MDTextField
+      :name="name"
+      :keyboardType="keyboardType"
+      :maxLength="maxLength"
+      :text="text"
+      :secure="secure"
+      :visibility="visibility"
+    />
+  </StackLayout>
+</template>
+
+<script>
+export default {
+  name: 'DataField',
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    keyboardType: {
+      type: String,
+      default: ''
+    },
+    maxLength: {
+      type: Number,
+      default: 0
+    },
+    text: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    secure: {
+      type: Boolean,
+      default: false
+    },
+    visibility: {
+      type: Boolean,
+      default: true
+    },
+    imageSrc: {
+      type: String,
+      required: true,
+      default: ''
+    }
+  },
+  data() {
+    return
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
