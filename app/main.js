663683092c1d520bba77fe0f64c9de2b0993f8e5
ignacio68 <ilopez-amor@ya.com>
2019-11-21 19:17:02 +0100

    IMPROVE: Update version number

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@
  * @copyright Ignacio López-Amor Pinillos 2019
  * @author Ignacio López-Amor Pinillos <ignaciolopezamor@gmail.com>
  * @license MIT
- * @version 0.1.1
+ * @version 0.2.1
  */
 
 import Vue from 'nativescript-vue'
