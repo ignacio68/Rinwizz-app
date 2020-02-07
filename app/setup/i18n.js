27d488a30ee589e0bb1b71922d6a540a12c98644
ignacio68 <ilopez-amor@ya.com>
2020-02-07 19:22:34 +0100

    Change alias paths

--- app/setup/i18n.js
+++ app/setup/i18n.js
@@ -7,18 +7,18 @@ import VueI18n from 'vue-i18n'
 /**
  * Import Locales
  */
-import enUs from '../locales/i18n/en_US'
-import esEs from '../locales/i18n/es_ES'
+import enUs from '@locales/i18n/en_US'
+import esEs from '@locales/i18n/es_ES'
 
 /**
  * Import number formats
  */
-import { numberFormats } from '../locales/i18n/numberFormats'
+import { numberFormats } from '@locales/i18n/numberFormats'
 
 /**
  * Import date formats
  */
-import { dateTimeFormats } from '../locales/i18n/dateTimeFormats'
+import { dateTimeFormats } from '@locales/i18n/dateTimeFormats'
 
 /**
  * Config
@@ -26,7 +26,7 @@ import { dateTimeFormats } from '../locales/i18n/dateTimeFormats'
 Vue.use(VueI18n)
 
 function loadLocaleMessages() {
-  const locales = require.context('../locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+  const locales = require.context('@locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
   const messages = {
     en: {
       lang: enUs
