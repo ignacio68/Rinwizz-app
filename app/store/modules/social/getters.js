9461eff0156ebfe7a5f915467cc0b8cafdc6202d
Ignacio <buscemi@ya.com>
2020-02-10 10:11:26 +0100

    Delete social buttons data

--- app/store/modules/social/getters.js
+++ app/store/modules/social/getters.js
@@ -1,5 +1,3 @@
-import { SOCIAL_BUTTONS } from '@store/types/getters_types'
+// import {} from '@store/types/getters_types'
 
-export default {
-  [SOCIAL_BUTTONS]: state => state.socialButtons
-}
+// export default {}
