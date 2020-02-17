89b7961bac314c3f90d311777f30829ea0950873
ignacio68 <ilopez-amor@ya.com>
2020-02-17 23:40:55 +0100

    Add logout and delete user buttons for production only

--- app/views/Main/Settings.vue
+++ app/views/Main/Settings.vue
@@ -1,24 +1,55 @@
 <template>
   <Page actionBarHidden="true">
     <StackLayout>
-      <label text="Settings" class="nt-drawer__header" />
+      <label
+        text="Settings"
+        class="nt-drawer__header"
+      />
     </StackLayout>
     <StackLayout>
-      <Label class="nt-drawer__list-item text" text="Perfil" textWrap="true" />
-      <Label class="nt-drawer__list-item text" text="Temas" textWrap="true" />
+      <Label
+        class="nt-drawer__list-item text"
+        text="Perfil"
+        textWrap="true"
+      />
+      <Label
+        class="nt-drawer__list-item text"
+        text="Temas"
+        textWrap="true"
+      />
       <Label
         class="nt-drawer__list-item text"
         text="Favoritos"
         textWrap="true"
       />
+      <Button
+        text="Log Out"
+        @tap="logOutUser"
+      />
+      <Button
+        text="Delete"
+        @tap="deleteUser"
+      />
     </StackLayout>
   </Page>
 </template>
 <script>
+import LogIn from '@views/Auth/Login'
+import Welcome from '@views/Welcome'
+import { mapActions } from 'vuex'
 export default {
   name: 'Settings',
   data() {
     return {}
+  },
+  methods: {
+    ...mapActions('auth', ['LOGOUT_USER', 'DELETE_USER']),
+    async logOutUser() {
+      await this.LOGOUT_USER().then(() => this.$navigateTo(LogIn))
+    },
+    async deleteUser() {
+      await this.DELETE_USER().then(() => this.$navigateTo(Welcome))
+    }
   }
 }
 </script>
