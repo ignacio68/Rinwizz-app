5161f93e0d4002cc3d4fb7bcd24b20624220007d
Ignacio <buscemi@ya.com>
2020-02-17 11:28:01 +0100

    Change USER_IS_VERIFIED to USER_IS_EMAIL_VERIFIED

--- app/store/modules/user/getters.js
+++ app/store/modules/user/getters.js
@@ -2,7 +2,7 @@ import {
   USER,
   USER_ID,
   USER_PROVIDER_ID,
-  USER_IS_VERIFIED,
+  USER_IS_EMAIL_VERIFIED,
   USER_FOLLOWING,
   USER_IS_NEW
 } from '@store/types/getters_types'
@@ -11,7 +11,7 @@ export default {
   [USER]: state => state.user,
   [USER_ID]: state => state.user._id,
   [USER_PROVIDER_ID]: state => state.user.providerID,
-  [USER_IS_VERIFIED]: state => state.user.isVerified,
+  [USER_IS_EMAIL_VERIFIED]: state => state.user.isEmailVerified,
   [USER_FOLLOWING]: state => state.user.following,
   [USER_IS_NEW]: state => state.user.isNewUser
 }
