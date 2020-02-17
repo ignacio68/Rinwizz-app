9beb40208a8898d8e2111b791b5905ea5dabdefc
Ignacio <buscemi@ya.com>
2020-02-17 13:31:27 +0100

    Add alerts local database module

---
+++ app/store/modules/localDb/alerts/getters.js
@@ -0,0 +1,5 @@
+import { GET_ALERTS_LOCAL_DB } from '@store/types/getters_types'
+
+export default {
+  [GET_ALERTS_LOCAL_DB]: state => state.alertsLocalDb
+}
---
+++ app/store/modules/localDb/alerts/state.js
@@ -0,0 +1,4 @@
+export default {
+  alertsLocalDb: {}, // base de datos de las alertas
+  userAlertsLocalD: {} // base de datos de las alertas del usuario
+}
