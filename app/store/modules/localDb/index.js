009576efd793e987f24ae9fdf4e82d569088ac3c
Ignacio <buscemi@ya.com>
2020-02-17 13:33:10 +0100

    Add user module

---
+++ app/store/modules/localDb/index.js
@@ -0,0 +1,10 @@
+// import alerts from './alerts'
+// import user from './user'
+// import users from './users'
+
+// export default {
+//   strict: process.env.NODE_ENV !== 'production',
+//   namespaced: true,
+//   //   alerts,
+//   users
+// }
